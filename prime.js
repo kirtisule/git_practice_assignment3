99c425e0c04dd1bee9f3e3ddff6c824c7944d058
keshav0605 <keshav06patel@gmail.com>
2022-11-06 22:36:52 +0530

    code fixed by m2

--- prime.js
+++ prime.js
@@ -1,6 +1,11 @@
 funtion check prime(num){
-let factor = 0;
+let count = 0;
 for(let i=1;i<=num; i++){
 if(num%i==0){
-factor++
+count++
+}if(count == 2){
+console.log("The Number is Prime Number");
+}else{
+console.log("The Number is not a Prime Number");
+}
 }
